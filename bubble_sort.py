bd9c4cd324133b19ad21b4005cd623f12f3bbfe6
Arjuna Aqa <arjunaaqa@gmail.com>
2025-04-26 09:38:44 +0000

    Added quick sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,41 @@
+import time
+
 def bubble_sort(array):
-    
+    length = len(array)
+    for i in range(length):
+        for j in range(0, length-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+
+def quick_sort(array):
+    if len(array) <=1:
+        return array
+    pivot = array[len(array) // 2]
+    left = [ x for x in array if x < pivot]
+    middle = [ x for x in array if x == pivot]
+    right = [ x for x in array if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+
 
 def main():
-    array = [1, 5, 7, 3]
+    array = [1, 11, 7, 3, 9, 2, 5]
+    start_time = time.perf_counter()
     result = bubble_sort(array)
-    print(f"Sorted array is {result}")
+    end_time = time.perf_counter()
+    print(f"Sorted array is {array}")
+    time_taken = float(end_time - start_time)
+    print(f"Bubble sort time taken is {time_taken: .10f} seconds")
+
+    array = [1, 11, 7, 3, 9, 2, 5]
+    start_time = time.perf_counter()
+    sorted_array = quick_sort(array)
+    end_time = time.perf_counter()
+    time_taken = float(end_time - start_time)
+    print(f"Sorted array is {sorted_array}")
+    print(f"Quick sort time taken is {time_taken:.10f} seconds")
 
 if __name__ == "__main__":
     main()
---
+++ quick_sort.py
@@ -0,0 +1,12 @@
+import time
+
+def quick_sort()::
+    pass
+
+
+def main():
+    
+    pass
+
+if __name__ == "__main__":
+    main()
